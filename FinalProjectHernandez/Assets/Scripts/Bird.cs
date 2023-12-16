ab72226148baa1a68d27bd7fb98fc23df584e325
Kenneth Hernandez <724914@student.dusd.net>
2023-12-16 12:45:18 -0800

    Finished the game.

--- FinalProjectHernandez/Assets/Scripts/Bird.cs
+++ FinalProjectHernandez/Assets/Scripts/Bird.cs
@@ -9,11 +9,16 @@ public class Bird : MonoBehaviour
     private bool isDead = false;
     private Rigidbody2D rb2d;
     private Animator anim;
+
+    AudioSource audioSource;
+    public AudioClip flapSound;
+    public AudioClip dieSound;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,13 +31,22 @@ public class Bird : MonoBehaviour
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce (new Vector2 (0, upForce));
                 anim.SetTrigger ("Flap");
+                PlaySound(flapSound);
             }
         }
     }
 
     void OnCollisionEnter2D()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger ("Die");
+        GameControl.instance.BirdDied();
+        PlaySound(dieSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
